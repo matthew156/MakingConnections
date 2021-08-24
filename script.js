0bdef880bfb51afe6e1a30efe5fb945b528a9ca6
matthew156 <matthewastro@gmail.com>
2021-08-24 10:10:54 -0400

    Making Connections 1st Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var textRemover = document.querySelector("#list1");
+
+function remove() {
+  console.log("test");
+  textRemover.innerHTML = "";
+}
+
+var textRemover2 = document.querySelector("#list2");
+
+function remove2() {
+  console.log("test");
+  textRemover2.innerHTML = "";
+}
+
+var editprofile = document.querySelector("#editprofile");
+
+function editProfile() {
+  console.log("test");
+  editprofile.innerHTML = "John Doe";
+}
